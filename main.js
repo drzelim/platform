611feac122a58de91ca44ca9c22dd1681e9d79e3
Zelimkhan <kastiel85@mail.ru>
2022-09-27 17:13:01 +0300

    Add js memory

--- main.js
+++ main.js
@@ -8,13 +8,15 @@ const h1 = document.querySelector('h1');
 const platformH2 = document.querySelector('#platform');
 const vendorH2 = document.querySelector('#vendor');
 const productSubH2 = document.querySelector('#productSub');
+const jsMemoryH2 = document.querySelector('#jsMemory');
 
 const platform = navigator.platform;
 const vendor = navigator.vendor;
 const productSub = navigator.productSub;
+const jsMemory = window.performance.memory.jsHeapSizeLimit;
 
 h1.textContent = 'Platform: ' + platform;
 platformH2.textContent = 'GPU info: ' + GPUInfo;
 vendorH2.textContent = 'Vendor: ' + vendor;
 productSubH2.textContent = 'ProductSub: ' + productSub;
-
+jsMemoryH2.textContent = 'jsMemory: ' + jsMemory;
