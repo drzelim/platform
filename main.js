00b0fcc5cbcd916320beaec0bbdf9e04a5da2b60
Zelimkhan <kastiel85@mail.ru>
2022-09-29 11:42:16 +0300

    ...

--- main.js
+++ main.js
@@ -11,13 +11,32 @@ const productSubH2 = document.querySelector('#productSub');
 const jsMemoryH2 = document.querySelector('#jsMemory');
 const oscpuH2 = document.querySelector('#oscpu');
 const pluginsH2 = document.querySelector('#plugins');
+const errorH2 = document.querySelector('#error');
+const batteryH2 = document.querySelector('#battery');
+const hiddenH2 = document.querySelector('#hidden');
 
 const platform = navigator.platform;
 const vendor = navigator.vendor;
 const productSub = navigator.productSub;
 const oscpu = navigator.oscpu;
 const plugins = navigator.plugins;
+const battery = navigator.getBattery;
 let jsMemory = '';
+
+function getHiddenFunc(d) {
+  try {
+    if (d.webkitHidden != undefined)
+      return "webkitHidden";
+    if (d.msHidden != undefined)
+      return "msHidden";
+    if (d.mozHidden != undefined)
+      return "mozHidden";
+    else
+      return "n/a";
+  } catch (_a) {
+    return "n/a";
+  }
+}
 
 try {
   jsMemory = window.performance.memory.jsHeapSizeLimit;
@@ -30,11 +49,20 @@ productSubH2.textContent = 'ProductSub: ' + productSub;
 jsMemoryH2.textContent = 'jsMemory: ' + jsMemory;
 oscpuH2.textContent = 'oscpu: ' + oscpu;
 pluginsH2.textContent = 'oscpu: ' + plugins;
+batteryH2.textContent = 'battery: ' + battery;
+hiddenH2.textContent = 'hiddenFunc: ' + getHiddenFunc(document);
 
-// 2172649472
-// 4294705152
-// 3760000000
-// 2190000000
 
-// 1136000000 - Mobile
-//
+const originalFunction = HTMLElement.offsetWidth;
+const span = document.createElement('span')
+
+HTMLElement.offsetWidth = function() {
+  console.log(this);
+  originalFunction.apply(this);
+}
+
+Object.defineProperty(HTMLElement, 'offsetWidth', {get: () => 11111});
+console.log(span.offsetWidth);
+console.log(span);
+
+["Andale Mono", "Arial", "Arial Black", "Arial Hebrew", "Arial MT", "Arial Narrow", "Arial Rounded MT Bold", "Arial Unicode MS", "Bitstream Vera Sans Mono", "Book Antiqua", "Bookman Old Style", "Calibri", "Cambria", "Cambria Math", "Century", "Century Gothic", "Century Schoolbook", "Comic Sans", "Comic Sans MS", "Consolas", "Courier", "Courier New", "Garamond", "Geneva", "Georgia", "Helvetica", "Helvetica Neue", "Impact", "Lucida Bright", "Lucida Calligraphy", "Lucida Console", "Lucida Fax", "LUCIDA GRANDE", "Lucida Handwriting", "Lucida Sans", "Lucida Sans Typewriter", "Lucida Sans Unicode", "Microsoft Sans Serif", "Monaco", "Monotype Corsiva", "MS Gothic", "MS Outlook", "MS PGothic", "MS Reference Sans Serif", "MS Sans Serif", "MS Serif", "MYRIAD", "MYRIAD PRO", "Palatino", "Palatino Linotype", "Segoe Print", "Segoe Script", "Segoe UI", "Segoe UI Light", "Segoe UI Semibold", "Segoe UI Symbol", "Tahoma", "Times", "Times New Roman", "Times New Roman PS", "Trebuchet MS", "Verdana", "Wingdings", "Wingdings 2", "Wingdings 3", ]
