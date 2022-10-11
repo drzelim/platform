ae7f71ec8af3c2bbf65e8ea9052c28e7f5a811af
Zelimkhan <kastiel85@mail.ru>
2022-10-11 22:06:20 +0300

    ...

--- js/main.js
+++ js/main.js
@@ -3,16 +3,21 @@ import './canvas-fp.js'
 
 const container = document.querySelector('.container');
 
-const canvas = document.createElement("canvas");
-const ctx =
-  canvas.getContext("webgl2") ||
-  canvas.getContext("experimental-webgl2") ||
-  canvas.getContext("webgl") ||
-  canvas.getContext("experimental-webgl") ||
-  canvas.getContext("moz-webgl");
-const renderer = ctx.getExtension("WEBGL_debug_renderer_info");
 let GPUInfo = null;
-renderer ? (GPUInfo = ctx.getParameter(renderer.UNMASKED_RENDERER_WEBGL)) : "";
+
+try {
+  const canvas = document.createElement("canvas");
+  const ctx =
+    canvas.getContext("webgl2") ||
+    canvas.getContext("experimental-webgl2") ||
+    canvas.getContext("webgl") ||
+    canvas.getContext("experimental-webgl") ||
+    canvas.getContext("moz-webgl");
+  const renderer = ctx.getExtension("WEBGL_debug_renderer_info");
+  renderer ? (GPUInfo = ctx.getParameter(renderer.UNMASKED_RENDERER_WEBGL)) : "";
+} catch {
+
+}
 
 
 const h1 = document.querySelector("h1");
